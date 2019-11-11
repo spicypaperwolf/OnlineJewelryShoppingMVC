76d5d320570337c783c86a73b936b68ec787bef5
diephn52 <diephn52@gmail.com>
2019-11-11 16:56:17 +0700

    client theme second

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJewelryShoppingMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Shop/Detail
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        // GET: Shop/Cart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        // GET: Shop/Checkout
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
