e174f9ea93fa0fad5f60a28e25fb795bc71bbe8b
Spicy Paper Wolf <40587351+spicypaperwolf@users.noreply.github.com>
2019-11-17 18:14:08 +0700

    Merge Unfinished Login from diephuynh

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace OnlineJewelryShoppingMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Client", action = "Login", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineJewelryShoppingMVC.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineJewelryShoppingMVC.Common;
+using OnlineJewelryShoppingMVC.Models;
 using OnlineJewelryShoppingMVC.Respository;
 
 namespace OnlineJewelryShoppingMVC.Controllers
 {
     public class ClientController : Controller
     {
+
+        OnlineJewelryShopDBEntities _context = new OnlineJewelryShopDBEntities();
         public ClientController()
         {
             ViewBag.TotalPrice = CartController.totalPrice;
@@ -46,10 +50,123 @@ namespace OnlineJewelryShoppingMVC.Controllers
             return View();
         }
 
-        //Login page for user
+        //Login page for user/admi
         public ActionResult Login()
         {
+            Session.Remove(CommonConstants.USER_SESSION);
             return View();
+        }
+
+        // 
+        public bool isAdmin(string userName, string passWord)
+        {
+            try
+            {
+                if (_context.AdminLoginMsts.First(item => item.username.Equals(userName) && item.password.Equals(passWord)) == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public UserRegMst GetByUserName(string userName)
+        {
+            return _context.UserRegMsts.SingleOrDefault(x => x.emailId == userName);
+        }
+        public AdminLoginMst GetByAdminName(string userName)
+        {
+            return _context.AdminLoginMsts.First(item => item.username.Equals(userName));
+        }
+
+        // Ham kiem tra login
+        public int checkLogin(string userName, string passWord)
+        {
+            var result = _context.UserRegMsts.SingleOrDefault(x => x.emailId == userName);
+            if (isAdmin(userName, passWord))
+            {
+                return 2;
+            }
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (result.password == passWord)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = checkLogin(model.emailId, (model.password));
+                if (result == 1)
+                {
+                    Session.Remove(CommonConstants.USER_SESSION);
+                    var user = GetByUserName(model.emailId);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.emailId; // user.UserName in Database
+                    userSession.Role = "user";
+
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Client");
+                }
+                else if (result == 2)
+                {
+                    Session.Remove(CommonConstants.USER_SESSION);
+                    AdminLoginMst user = GetByAdminName(model.emailId);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.username; // user.UserName in Database
+                    userSession.Role = "admin";
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Dashboard", "Admin/Admin");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Login is invalid!");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Account is being blocked!");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Password is not correct");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login is not correct!");
+                }
+            }
+            return View("Login");
+        }
+        // Logout function
+        public ActionResult Logout()
+        {
+            Session[CommonConstants.USER_SESSION] = null;
+            return RedirectToAction("Index", "Client");
         }
 
         //Cart Page for detailed information in cart
